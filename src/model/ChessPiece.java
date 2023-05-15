32816ba9426b726007bd11d2a2945e05446293c7
WuHongli2131 <12210710@mail.sustech.edu.cn>
2023-05-15 21:21:44 +0800

    init commit

---
+++ src/model/ChessPiece.java
@@ -0,0 +1,36 @@
+package model;
+
+
+public class ChessPiece {
+    // the owner of the chess
+    private PlayerColor owner;
+
+    // Elephant? Cat? Dog? ...
+    private String name;
+    private int rank;
+
+    public ChessPiece(PlayerColor owner, String name, int rank) {
+        this.owner = owner;
+        this.name = name;
+        this.rank = rank;
+    }
+
+    public boolean canCapture(ChessPiece target) {
+        // TODO: Finish this method!
+        if(this.rank==1&& target.rank==8){
+            return true;
+        }
+        if(this.rank>= target.rank){
+            return true;
+        }
+        return false;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public PlayerColor getOwner() {
+        return owner;
+    }
+}
